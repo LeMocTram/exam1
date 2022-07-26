499d7e91b977b9c013203541b94b8644b1ed0c3d
Moc Tram <19522373@gm.uit.edu.vn>
2022-07-26 15:35:37 +0700

    first commit moctram

---
+++ edittext/edittext/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace edittext
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            FileStream fs = new FileStream(ofd.FileName, FileMode.OpenOrCreate);
+            StreamReader sr = new StreamReader(fs);
+
+            richTextBox1.Text = sr.ReadToEnd();
+            fs.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveMyFile();
+        }
+
+        public void SaveMyFile()
+        {
+            // Create a SaveFileDialog to request a path and file name to save to.
+            SaveFileDialog saveFile1 = new SaveFileDialog();
+
+            // Initialize the SaveFileDialog to specify the RTF extention for the file.
+            saveFile1.DefaultExt = "*.rtf";
+            saveFile1.Filter = "RTF Files|*.rtf";
+
+            // Determine whether the user selected a file name from the saveFileDialog.
+            if (saveFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+               saveFile1.FileName.Length > 0)
+            {
+                // Save the contents of the RichTextBox into the file.
+                richTextBox1.SaveFile(saveFile1.FileName);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                System.Drawing.Font currentFont = richTextBox1.SelectionFont;
+                System.Drawing.FontStyle newFontStyle;
+
+                if (richTextBox1.SelectionFont.Bold == true)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+                else
+                {
+                    newFontStyle = FontStyle.Bold;
+                }
+
+                richTextBox1.SelectionFont = new Font(
+                   currentFont.FontFamily,
+                   currentFont.Size,
+                   newFontStyle
+                );
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string choose = this.comboBox3.SelectedItem.ToString();
+            this.richTextBox1.SelectionFont = new Font(choose, this.richTextBox1.Font.Size, this.richTextBox1.Font.Style);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Yellow")
+            {
+
+                richTextBox1.SelectionColor = Color.Yellow;
+            }
+            if (comboBox1.Text == "Red")
+            {
+
+                richTextBox1.SelectionColor = Color.Red;
+            }
+            if (comboBox1.Text == "Orange")
+            {
+
+                richTextBox1.SelectionColor = Color.Orange;
+            }
+            if (comboBox1.Text == "Blue")
+            {
+
+                richTextBox1.SelectionColor = Color.Blue;
+            }
+            if (comboBox1.Text == "Black")
+            {
+
+                richTextBox1.SelectionColor = Color.Black;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            float choose = Convert.ToSingle(this.comboBox2.SelectedItem.ToString());
+            this.richTextBox1.SelectionFont = new Font(this.richTextBox1.Font.FontFamily, choose, this.richTextBox1.Font.Style);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                System.Drawing.Font currentFont = richTextBox1.SelectionFont;
+                System.Drawing.FontStyle newFontStyle;
+
+                if (richTextBox1.SelectionFont.Italic == true)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+                else
+                {
+                    newFontStyle = FontStyle.Italic;
+                }
+
+                richTextBox1.SelectionFont = new Font(
+                   currentFont.FontFamily,
+                   currentFont.Size,
+                   newFontStyle
+                );
+            }
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                System.Drawing.Font currentFont = richTextBox1.SelectionFont;
+                System.Drawing.FontStyle newFontStyle;
+
+                if (richTextBox1.SelectionFont.Underline == true)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+                else
+                {
+                    newFontStyle = FontStyle.Underline;
+                }
+
+                richTextBox1.SelectionFont = new Font(
+                   currentFont.FontFamily,
+                   currentFont.Size,
+                   newFontStyle
+                );
+            }
+        }
+    }
+}
